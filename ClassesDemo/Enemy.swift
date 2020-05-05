7120ca34f5aac240ebc91ab2bf3fd0f41f509913
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-05 13:27:59 -0400

    Finish

--- ClassesDemo/Enemy.swift
+++ ClassesDemo/Enemy.swift
@@ -7,9 +7,16 @@
 //
 
 class Enemy {
-    var health = 100
-    var attackStrenght = 10
+    var health: Int
+    var attackStrenght:Int
     
+    init(health: Int, attackStrenght: Int) {
+        self.health = health
+        self.attackStrenght = attackStrenght
+    }
+    func takeDamage(amount: Int) {
+        health = health - amount
+    }
     func move() {
         print("Walk forwards.")
     }
--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2020 Iurie Guzun. All rights reserved.
 //
 
-let skeleton = Enemy()
+let skeleton1 = Enemy(health: 100, attackStrenght: 10)
+let skeleton2 = Enemy(health: 100, attackStrenght: 10)
+
+skeleton1.takeDamage(amount: 10)
+print(skeleton2.health)
 
 
-let dragon = Dragon()
-dragon.wingSpan = 5
-dragon.attackStrenght = 15
-dragon.talk(speech: "My teeth are swords!" )
-dragon.move()
-dragon.attack()
+
+//let dragon = Dragon()
+//dragon.wingSpan = 5
+//dragon.attackStrenght = 15
+//dragon.talk(speech: "My teeth are swords!" )
+//dragon.move()
+//dragon.attack()
