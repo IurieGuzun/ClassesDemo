f03dc836bf4e70aefee21d31ae869869a36bb0cb
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-05 11:30:13 -0400

    Initial Commit

---
+++ ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassesDemo
+//
+//  Created by Iurie Guzun on 2020-05-05.
+//  Copyright © 2020 Iurie Guzun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
